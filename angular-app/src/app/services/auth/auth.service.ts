4afb09c0e7c3ebea4f6b72bbedfa98cdf8750385
alexmncn78 <118453946+alexmncn78@users.noreply.github.com>
2024-06-13 23:27:49 +0200

    Fix typo. Envs

--- angular-app/src/app/services/auth/auth.service.ts
+++ angular-app/src/app/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
-import { environment } from '../../../enviroments/enviroment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
