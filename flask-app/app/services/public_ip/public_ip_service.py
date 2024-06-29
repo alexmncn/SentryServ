b6c8f4613a68cf5b584ad0671f1c045695bd5bc7
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-06-29 16:41:59 +0200

    Update public_ip_service.py

--- flask-app/app/services/public_ip/public_ip_service.py
+++ flask-app/app/services/public_ip/public_ip_service.py
@@ -11,16 +11,21 @@ sys.path.append(app_path)
 from app.services.net_and_connections import get_public_ip
 from app.services.pushover_notifications import send_noti
 
-init = True
-
+last_public_IP = None
 
 def notify_new_public_ip():
-    public_ip = get_public_ip()
-    if public_ip is not None:
-        send_noti(f'IP del servidor: {public_ip}','default')
-    else:
-        time.sleep(10000)
-        notify_new_public_ip()
+    global last_public_IP
+    while True:
+        public_IP = get_public_ip()
+        
+        if public_IP is not None:
+            if public_IP != last_public_IP:
+                last_public_IP = public_IP
+                send_noti(f'IP del servidor: {public_IP}','default')
+            time.sleep(3600)
+        else:
+            time.sleep(60)
+        
 
 
 def main():
